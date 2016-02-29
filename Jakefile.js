251f111dd0800351cfe39e6a1437373c0fe54fc8
Fahim <phantom@khan.(none)>
2016-03-01 01:39:46 +0530

    Build Script Check the Node Version

--- Jakefile.js
+++ Jakefile.js
@@ -2,10 +2,22 @@
 (function(){
     "use strict";
     
+    var EXPECTED_NODE_VERSION = "v0.12.4";
+
     desc("Default Build");
-    task("default",function(){
+    task("default",[ "version" ],function(){
         console.log("\n\nBUILD OK");
 
+    });
+    
+    desc("Check Node Version")
+    task("version",function(){
+    console.log("Checking Node Version: .");
+    
+    var actualVersion = process.version;
+    if (actualVersion !== EXPECTED_NODE_VERSION) {
+        fail("Incorrect Node Version : expected node version : "+EXPECTED_NODE_VERSION+" but was : "+actualVersion);
+    }
     });
 
 }());
