cd9bf70d63824151c8b4528e118f271189b0c1de
Fahim <phantom@khan.(none)>
2016-03-02 22:24:20 +0530

    Started Working On Karma

--- src/test.js
+++ src/test.js
@@ -1,24 +1,26 @@
-(function(){
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
 
-    "use strict";
-        
-    describe("Addition", function() {
-        it("adds positive numbers", function() {
-            assertEqual(add(3, 4), 7);
-        });
-        it("uses IEEE 754 floating point", function() {
-           assert.equal(add(0.1, 0.2), 0.30000000000000004);
-        });
+	describe("Addition", function() {
 
-        function assertEqual(actual, expected) {
-                        if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
-                                }
-    
-    });
+		it("adds positive numbers", function() {
+			assertEqual(add(3, 4), 7);
+		});
 
-    function add(a, b) {
-        return a + b;
-    }
-    
+		it("uses IEEE 754 floating point", function() {
+			assertEqual(add(0.1, 0.2), 0.30000000000000004);
+		});
 
-})();
+		function assertEqual(actual, expected) {
+			if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
+		}
+
+	});
+
+
+	function add(a, b) {
+		return a + b;
+	}
+
+}());
