02f96d7c2090d1d2d105c5856dbe95796819aa62
Fahim <phantom@khan.(none)>
2016-03-04 23:19:01 +0530

    Build task now use broserify to generate build.js

--- Jakefile.js
+++ Jakefile.js
@@ -83,6 +83,10 @@
 		console.log("Building Distribution Directory: .");
 		shell.rm("-rf",DIST_DIR+"/*");
 		shell.cp("src/index.html",DIST_DIR);
+
+		jake.exec("node node_modules/browserify/bin/cmd.js  src/app.js -o "+DIST_DIR+"/bundle.js",
+		 { interactive: true },
+		  complete);
 	});
 
 
