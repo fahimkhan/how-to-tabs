edaf2b899cf044402401836d12bdfb151b5afd1b
fahimkhan <khanfahim2000@yahoo.com>
2016-03-09 23:46:04 +0530

    E27-Added option objects

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -4,14 +4,20 @@
 	var classList = require("../vendor/classList.js");
 	classList.shim();
 
-	exports.initialize = function initialize(elementList,className){
-		elementList.forEach(function(element){
-			element.classList.add(className);
+	exports.initialize = function initialize(options){
+		var content = options.content;
+		var defaultElement = options.default;
+		var contentHideClass = options.contentHideClass;
+		
+		if (content === undefined) throw new Error("Expected options.content");
+		if (defaultElement === undefined) throw new Error("Expected options.default");
+		if (contentHideClass === undefined) throw new Error("Expected options.contentHideClass");
+		
+		content.forEach(function(element){
+		element.classList.add(contentHideClass);
 		});
 
-		var element = elementList[0];
-		element.classList.add(className);
-	
+		defaultElement.classList.remove(contentHideClass);
 	};
 
 
