b313764a20253163ddb750e412cc5da94093e462
fahimkhan <khanfahim2000@yahoo.com>
2016-03-10 22:21:00 +0530

    E30-What Should you test

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -7,31 +7,28 @@
 	classList.shim();
 
 	exports.initialize = function initialize(options) {
-		var tabs = options.tabs;
-		var content = options.content;
-		var defaultTab = options.defaultTab;
-		var activeTabClass = options.activeTabClass;
-		var hiddenContentClass = options.hiddenContentClass;
+		checkOption(options.tabs, "options.tabs");
+		checkOption(options.content, "options.content");
+		checkOption(options.defaultTab, "options.defaultTab");
+		checkOption(options.activeTabClass, "options.activeTabClass");
+		checkOption(options.hiddenContentClass, "options.hiddenContentClass");
 
-		checkOption(tabs, "options.tabs");
-		checkOption(content, "options.content");
-		checkOption(defaultTab, "options.defaultTab");
-		checkOption(activeTabClass, "options.activeTabClass");
-		checkOption(hiddenContentClass, "options.hiddenContentClass");
-
-
-		var activeIndex = findIndexOfDefaultElement(tabs, defaultTab);
-		var defaultContent = content[activeIndex];
-
-		content.forEach(function(element) {
-			element.classList.add(hiddenContentClass);
-		});
-
-		defaultContent.classList.remove(hiddenContentClass);
-		defaultTab.classList.add(activeTabClass);
+		showTab(options.defaultTab, options);
 	};
 
-	function findIndexOfDefaultElement(contentTabs, defaultContentTab) {
+	function showTab(tabToShow, options) {
+		var activeIndex = findIndex(options.tabs, tabToShow);
+		var contentToShow = options.content[activeIndex];
+
+		options.content.forEach(function(element) {
+			element.classList.add(options.hiddenContentClass);
+		});
+
+		contentToShow.classList.remove(options.hiddenContentClass);
+		tabToShow.classList.add(options.activeTabClass);
+	}
+
+	function findIndex(contentTabs, defaultContentTab) {
 		for (var i = 0; i < contentTabs.length; i++) {
 			if (contentTabs[i] === defaultContentTab) return i;
 		}
